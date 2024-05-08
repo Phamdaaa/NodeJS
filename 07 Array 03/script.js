1a77305ef1b7a90a90a044ffbbc4ab1d3d5eb5ef
Phamdaaa <francocioffi751@gmail.com>
2024-05-08 05:21:21 -0300

    .

--- 07 Array 03/script.js
+++ 07 Array 03/script.js
@@ -1,14 +1,17 @@
-function transformaNumeros(numeros){
+function transformaNums(numeros){
     var numero = "";
     var nuevoNumero = []
     for(var i = 0; i < numeros.length; i++){
-        if(numeros[i] != " "){
+        if (numeros[i] != " "){
             numero += numeros[i]
         }
         else{ 
             nuevoNumero.push(numero)
             numero = ""
         }
+    }
+    if (numero != "") {
+        nuevoNumero.push(numero);
     }
     return nuevoNumero;
 }
@@ -24,8 +27,36 @@ function filtraNum(numeros){
     return resultado;
 }
 
-numeros = "13 101 28883 13 345673 55 9 110 101 "
-nuevosNumeros = transformaNumeros(numeros)
-console.log(nuevosNumeros)
-resultado = filtraNum(nuevosNumeros)
-console.log(resultado)
+document.addEventListener('DOMContentLoaded', function () {
+    const Input = document.getElementById('Input');
+    const Button = document.getElementById('Button');
+
+    Button.addEventListener('click', function () {
+        const file = Input.files[0];
+
+        if (file) {
+            const reader = new FileReader();
+
+            reader.onload = function(event) {
+                const entrada = event.target.result;
+                console.log('Contenido del archivo:', entrada);
+                var numeros = transformaNums(entrada)
+                var salida = filtraNum(numeros)
+                const txt = new Blob([salida], { type: 'text/plain' });
+                const url = window.URL.createObjectURL(txt);
+                const a = document.createElement('a');
+                a.href = url;
+                a.download = 'salida.txt';
+                document.body.appendChild(a);
+                a.click();
+                window.URL.revokeObjectURL(url);
+                document.body.removeChild(a);
+                
+            };
+
+            reader.readAsText(file);
+        } else {
+            console.error('No se seleccionó ningún archivo.');
+        }
+    });
+});
